735bc9ff526409690d1b14c3983db8d2906dfb5b
magneto.1990@gmail.com <nexus@123>
2016-03-30 15:46:13 +0530

    mutileple selection issue fixes

--- js/appLib.js
+++ js/appLib.js
@@ -445,11 +445,9 @@ function fetchExpenseClaim() {
 			j("#source tr").click(function(){ 
 				headerOprationBtn = defaultPagePath+'headerPageForBEOperation.html';
 				if(j(this).hasClass("selected")){ 
-				var pageRef=defaultPagePath+'fairClaimTable.html';
 				var headerBackBtn=defaultPagePath+'headerPageForBEOperation.html';
 					j(this).removeClass('selected');
 					j('#mainHeader').load(headerBackBtn);
-					j('#mainContainer').load(pageRef);
 				}else{
 				if(j(this).text()=='DateExpense NameNarration From/To LocAmt'){
 					
@@ -541,11 +539,9 @@ function fetchExpenseClaim() {
 			j("#source tr").click(function(){
 				headerOprationBtn = defaultPagePath+'headerPageForTSOperation.html';
 				if(j(this).hasClass("selected")){ 
-				var pageRef=defaultPagePath+'travelSettlementTable.html';
 				var headerBackBtn=defaultPagePath+'headerPageForTSOperation.html';
 					j(this).removeClass('selected');
 					j('#mainHeader').load(headerBackBtn);
-					j('#mainContainer').load(pageRef);
 				}else{
 					if(j(this).text()=='DateExpense NameAmtcityTownNarration'){
 						
@@ -1105,7 +1101,6 @@ function fetchWalletImage() {
 						var pageRef=defaultPagePath+'addToWallet.html';
 							j(this).removeClass('selected');
 							j('#mainHeader').load(headerOprationBtn);
-							j('#mainContainer').load(pageRef);
 						}else{
 							j('#mainHeader').load(headerOprationBtn);
 							j(this).addClass('selected');					
